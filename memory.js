89b45e7e42ee1c8b71842518fef33c6bddb2134d
Alexandra Campo <alexandracampo86@gmail.com>
2023-05-24 21:50:10 +0200

    change confeti function

--- memory.js
+++ memory.js
@@ -42,12 +42,37 @@ function flipCard() {
 
 //Función efecto confeti:
 function generateConfetti() {
-  confetti({
-    particleCount: 800, // Ajusta el número de confetis generados
-    spread: 360,
-    origin: { y: 0.6 },
-    colors: ['#FFC0CB', '#FF69B4', '#FF1493', '#C71585'],
-  });
+  const duration = 15 * 1000,
+    animationEnd = Date.now() + duration,
+    defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
 }
 
 // Definimos la función que verifica el match de las tarjetas:
